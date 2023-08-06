671514245c4d20572f898b5bb2f0249d6fc52510
Abdurahman Abdelgany <abdu.abdelgany@hashicorp.com>
2023-08-05 22:48:54 -0400

    init

---
+++ main.tf
@@ -0,0 +1,51 @@
+terraform {
+    required_providers {
+        random = {
+            source = "hashicorp/random"
+            version = "~> 3.4"
+        }
+    }
+}
+
+variable "number" {
+    type = number
+    nullable = false
+    default = 1
+}
+
+variable "length" {
+    type = number
+    nullable = false
+    default = 1
+
+    validation{
+        condition = var.length > 0
+        error_message = "length must be positive"
+    }
+}
+
+variable "prefixes" {
+    type = list(string)
+    nullable = false
+    default = []
+}
+
+locals {
+    prefix = length(var.prefixes) == 0? null : join("-",var.prefixes)
+}
+
+resource "random_pet" "pet" {
+   count = var.number
+   length = var.length
+   prefix = local.prefix
+
+   lifecycle {
+    precondition {
+        condition = local.prefix != ""
+        error_message = "prefix can't be an empty string!"
+    }
+   } 
+}
+output "names" {
+    value = [for pet in random_pet.pet : pet.id]
+}
