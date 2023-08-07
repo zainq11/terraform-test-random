b762e4fc7c47e829ad04ace2a1f107f395fe89c4
Abdurahman Abdelgany <42511430+aaabdelgany@users.noreply.github.com>
2023-08-07 10:54:08 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ variable "length" {
 
     validation{
         condition = var.length > 0
-        error_message = "length must be positive plz"
+        error_message = "length must be positive"
     }
 }
 
